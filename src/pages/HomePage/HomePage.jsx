434983c12ae8883254ca0ad92178875c235843c4
mantezana <maximo00antezana@gmail.com>
2022-03-28 17:00:57 -0700

    bootstrap

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,22 @@
 import Footer from '../../components/Footer/Footer'
+import './HomePage.css'
 
 export default function HomePage(){
     return(
     <>
         <body>
             <header>
-                <h1>Start the journey to happiness and wellness with outcome-based autism care.</h1>
-                <p>Delivering data collection to help your loved one achieve their goals.</p>
-                <button>Get Started</button>
+                <div id="homePageHeader">
+                    <div class="left">
+                        <h1>Start the journey to happiness and wellness with outcome-based autism care.</h1>
+                        <p>Delivering data collection to help your loved one achieve their goals.</p>
+                        <button>Get Started</button>
+                    </div>
 
-                <img src="https://i.imgur.com/JK1D7Lz.jpg" alt="adult guiding kid while she draws" height="600px" width="900px"/>
+                    <div class="right">
+                        <img src="https://i.imgur.com/JK1D7Lz.jpg" alt="adult guiding kid while she draws" height="400px" width="600px"/>
+                    </div>
+                </div>
             </header>
         </body>
         <Footer />

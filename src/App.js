ab4086b673690243cdd984a2081cba1a30dd4119
mantezana <maximo00antezana@gmail.com>
2022-03-31 15:13:39 -0700

    about us and contact page created and routes working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from '../src/pages/HomePage/HomePage';
 import Measurements from '../src/pages/Measurements/Measurements';
 import Layout from '../src/pages/Layout/Layout';
+import AboutUs from '../src/pages/AboutUs/AboutUs';
+import ContactUs from '../src/pages/ContactUs/ContactUs';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />}/>
-        <Route path="/measurements" element={<Measurements />}/>
+        <Route path='aboutus' element={<AboutUs />}/>
+        <Route path="contactus" element={<ContactUs />}/>
+        <Route path="measurements" element={<Measurements />}/>
       </Route>
     </Routes>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,29 +4,29 @@ import { MDBBtn } from 'mdb-react-ui-kit';
 export default function Header (){
     return (
         <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Journey</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                    <Nav.Link href="">About Us</Nav.Link>
-                    <Nav.Link href="">Solutions</Nav.Link>
-                    <Nav.Link href="">Contact Us</Nav.Link>
-                    <NavDropdown title="Features" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="">Record Behaviors</NavDropdown.Item>
-                        <NavDropdown.Item href="">See Progress</NavDropdown.Item>
-                        <NavDropdown.Item href="">Graph progress</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="">See Sample Sheet</NavDropdown.Item>
-                    </NavDropdown>
-                    </Nav>
-                    <Nav>
-                    <MDBBtn>Start Journey</MDBBtn>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Journey</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        <Nav.Link href="">About Us</Nav.Link>
+                        <Nav.Link href="">Solutions</Nav.Link>
+                        <Nav.Link href="">Contact Us</Nav.Link>
+                        <NavDropdown title="Features" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="">Record Behaviors</NavDropdown.Item>
+                            <NavDropdown.Item href="">See Progress</NavDropdown.Item>
+                            <NavDropdown.Item href="">Graph progress</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="">See Sample Sheet</NavDropdown.Item>
+                        </NavDropdown>
+                        </Nav>
+                        <Nav>
+                        <MDBBtn>Start Journey</MDBBtn>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     )
 }

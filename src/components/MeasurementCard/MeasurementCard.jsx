eb579b4a32ed4a856eed601cb8375af8ee9cc59b
mantezana <maximo00antezana@gmail.com>
2022-03-31 17:19:51 -0700

    measurements looped into cards component

--- src/components/MeasurementCard/MeasurementCard.jsx
+++ src/components/MeasurementCard/MeasurementCard.jsx
@@ -3,21 +3,23 @@ import { Card, Button } from 'react-bootstrap';
 export default function MeasurementCard (){
 
     const measurements = [
-        "Frequency",
-        "Duration", 
-        "Latency Recording", 
-        "Interval Recording"
+        {measurementName: "Frequency"},
+        {measurementName: "Duration"}, 
+        {measurementName: "Latency Recording"}, 
+        {measurementName: "Interval Recording"}
     ]
 
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{showMeasurements()}</Card.Title>
+            {measurements.map((meas) => (
+                <Card.Body>
+                <Card.Title>{meas.measurementName}</Card.Title>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of the card's content.
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
+            ))}
         </Card>
     )
 }
--- src/pages/Measurements/Measurements.jsx
+++ src/pages/Measurements/Measurements.jsx
@@ -3,10 +3,6 @@ import MeasurementCard from "../../components/MeasurementCard/MeasurementCard";
 export default function Measurements(){
     return(
     <>
-        <h1>Frequency</h1>
-        <h1>Duration</h1>
-        <h1>Latency Recording</h1>
-        <h1>Interval Recording</h1>
         <MeasurementCard />
     </>
     )

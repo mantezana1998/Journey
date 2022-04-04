8e9df584f1979c51d78c2446d6215f461da89ce1
mantezana <maximo00antezana@gmail.com>
2022-04-04 12:41:32 -0700

    tabs and font added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Button, Tabs, Tab } from "react-bootstrap";
 import './HomePage.css';
+// import { BsFillHandIndexFill } from "react-icons/bs";
 
 export default function HomePage(){
     return(
@@ -21,17 +22,31 @@ export default function HomePage(){
                 </Row>
             </Container>
 
-            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-                <Tab eventKey="home" title="Home">
-                    Hello World!
-                </Tab>
-                <Tab eventKey="profile" title="Profile">
-                    Hello World!
-                </Tab>
-                <Tab eventKey="contact" title="Contact">
-                    Hello World!
-                </Tab>
-            </Tabs>
+            <div id="tab-groups">
+                <h1>How it works</h1>
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
+                    <Tab eventKey="home" title="Create Account">
+                        <h3>Sign up for free and record all your data.</h3>
+                        <p>See the progress </p> 
+                    </Tab>
+                    <Tab eventKey="record data" title="Record Data" BsFillBugFill>
+                        <h3>Record your data and see your loved ones progress.</h3>
+                        <p>See the progress </p> 
+                    </Tab>
+                    <Tab eventKey="graph data" title="Graph Data">
+                        <h3>Graph data on a chart.</h3>
+                        <p>See the progress </p> 
+                    </Tab>
+                    <Tab eventKey="send data" title="Send Data">
+                        <h3>Send data to your ABA services or therapist.</h3>
+                        <p>See the progress </p> 
+                    </Tab>
+                    <Tab eventKey="see progress" title="See Progress">
+                        <h3>See the numbers on how much progress your loved one has done.</h3>
+                        <p>See the progress </p> 
+                    </Tab>
+                </Tabs>
+            </div>
         </main>
     </>    
     )
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import './Layout.css';
 
 export default function Layout(){
     return(

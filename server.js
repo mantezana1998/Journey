89ab8fea9bcdaf1d5e5b033d3e0348f9dd750e1b
mantezana <maximo00antezana@gmail.com>
2022-04-26 16:44:18 -0700

    database connected

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+require('./config/database');
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function(){
+    console.log(`Express app listening on port ${port}`);
+});

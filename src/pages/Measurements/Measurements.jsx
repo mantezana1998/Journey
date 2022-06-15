bd6a75813ac07ad8b1c3f935bc25235aad971b70
ditabu <dibueno@gmail.com>
2022-06-15 12:05:28 -0700

    cards feed

--- src/pages/Measurements/Measurements.jsx
+++ src/pages/Measurements/Measurements.jsx
@@ -1,10 +1,13 @@
 import MeasurementCard from "../../components/MeasurementCard/MeasurementCard";
+import CardsFeed from "../../components/CardsFeed/CardsFeed";
 import './Measurements.css';
 
 export default function Measurements(){
     return(
     <>
-        <MeasurementCard />
+      
+        <CardsFeed/>
+       
     </>
     )
 }

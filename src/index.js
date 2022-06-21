e44cc7388aa45cda314c327f909d2584a7ccfdfa
mantezana <maximo00antezana@gmail.com>
2022-06-20 19:55:08 -0700

    signup and login complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from '../src/pages/App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';

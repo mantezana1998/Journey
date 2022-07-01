eb636c7d430e193573567065df69e05d2ea99c85
mantezana <maximo00antezana@gmail.com>
2022-06-30 19:37:02 -0700

    contact form completed

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,28 +1,95 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
 import './ContactUs.css';
-import { BsFillTelephoneFill } from 'react-icons/bs'
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { useForm } from '@formspree/react';
+import { useState } from 'react';
+import ReactJsAlert from "reactjs-alert"
 
 export default function ContactUs(){
-    return (
-    <Container>
-        <Row>
-            <Col>
-                <div className="contact-info">
-                    <h1>Contact us</h1>
-                    <p>Feel free to get in touch with the Journey Support Team. We're always open to discussing new projects, creative ideas, or opportunities to collaborate</p>
-                    <p>Call us at <BsFillTelephoneFill /> +1 123 456 7890</p>
-                </div>
-            </Col>
 
-            <Col>
-                <div className="contact-forms">
-                    <textarea id="textarea" cols="30" rows="1" placeholder="Enter your Name"></textarea>
-                    <textarea id="textarea" cols="30" rows="1" placeholder="Enter a valid email address"></textarea>
-                    <textarea id="textarea" cols="30" rows="5" placeholder="Enter your message"></textarea>
-                    <Button variant="primary" size="lg">Submit</Button>
+    const [state, handleSubmit] = useForm("xvolnjwr");
+    const [status, setStatus] = useState(false);
+    const [type, setType] = useState("success");
+    const [title, setTitle] = useState("");
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    function handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    function alertUser(){
+        setStatus(true)
+        setType("success");
+        setTitle("Your email has been sent!");
+    }
+
+    return (
+        <>
+            <ReactJsAlert
+            status={status} // true or false
+            type={type} // success, warning, error, info
+            title={title}
+            Close={() => setStatus(false)}
+            />
+            <div className="contact-form">
+                <div className="contact-card">
+                    <h1>Contact US</h1>
+                    <p className="contact-p">Feel free to get in touch with the Journey Support Team. We're always open to discussing new projects, creative ideas, or opportunities to collaborate</p>
+                    <p className="contact-p">Call us at <BsFillTelephoneFill /> +1 123 456 7890</p>
                 </div>
-            </Col>
-        </Row>
-    </Container>
+
+                <div className="contact-form-card">
+                    <form onSubmit={handleSubmit}>
+                        <p className="contact-form-p">Enter your name</p>
+                        <input 
+                            type="text" 
+                            className="contact-form-input-box" 
+                            name="name" 
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="Ex. John Doe" 
+                            required 
+                        />
+
+                        <p className="contact-form-p">Enter a valid email address</p>
+                        <input 
+                            type="text" 
+                            className="contact-form-input-box" 
+                            name="email" 
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="Ex. JohnDoe123@email.com" 
+                            required 
+                        />
+                        
+                        <p className="contact-form-p">Enter your message</p>
+                        <textarea 
+                            className="contact-form-input-box" 
+                            cols="30" 
+                            rows="4" 
+                            name="message" 
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="Ex. Let's Collaborate!">
+                        </textarea>
+                        
+                        <button 
+                            type="submit" 
+                            className="button" 
+                            onClick={alertUser}>
+                                Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }

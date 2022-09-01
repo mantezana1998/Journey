7c80a51f678974c29269d0a0c2879fe8fff393ff
mantezana <maximo00antezana@gmail.com>
2022-09-01 14:19:24 -0700

    id working

--- controllers/graphs.js
+++ controllers/graphs.js
@@ -6,7 +6,7 @@ module.exports = {
 
 async function showGraph(req, res){
     try{
-        const showBehaviorId = await Behavior.findOne({ _id: req.params.id }).populate('user').exec()
+        const showBehaviorId = await Behavior.findById({ _id: req.params.id }).populate('user').exec()
         res.status(200).json({ showBehaviorId: showBehaviorId })
       }catch(err){
         res.status(400).json({ err })

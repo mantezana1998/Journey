f6e97c189c274ebb2a3c8db8089bac11ebf709a1
mantezana <maximo00antezana@gmail.com>
2022-09-07 14:00:57 -0700

    models complete

--- models/graph.js
+++ models/graph.js
@@ -1,6 +1,13 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
-// const graphSchema = new mongoose.Schema({
-//     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+const graphSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    behavior: {type: mongoose.Schema.Types.ObjectId, ref: 'Behavior'},
+    typeOfBehavior: {type: String},
+    time: {type: Date},
+    occurrences: {type: Number}
+}, {
+    timestamps: true
+})
 
-// })
+module.exports = mongoose.model('Graph', graphSchema);
